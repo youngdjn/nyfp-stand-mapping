63634f8e722592fb5c09556fca35d77dab6daad5
Derek <derek.j.n.young@gmail.com>
2021-10-01 18:47:42 -0700

    Stem map grid creation working

---
+++ scripts/make_stemmap_grids.R
@@ -0,0 +1,98 @@
+library(sf)
+library(dplyr)
+
+pt_df = data.frame(lat = c(   37.74,    37.75),
+                   lon = c(-119.159, -119.169),
+                   name = c("A","B"))
+
+# pt_df = data.frame(lat = c(   37.74),
+#                    lon = c(-119.159)
+# )
+
+pt = st_as_sf(pt_df, coords = c("lon","lat"),crs=4326)
+
+
+
+points_albers = st_transform(pt,3310)
+
+
+# allcorners = NA
+
+for(i in 1:nrow(points_albers)) {
+  
+  point = points_albers[i,]
+  stemmap_name = point$name
+  
+  # make a circle with 60.01 m radius, then the square that encompases it
+  circle = st_buffer(point,40)
+  square = st_make_grid(circle,cellsize = 161.0,what = "polygons")
+  
+  corners = st_make_grid(square,cellsize = 20,what = "corners")
+  centers = st_make_grid(square,cellsize = 20,what = "centers")
+  
+  # if(is.na(allcorners)) {
+    allcorners = corners
+    allcenters = centers
+  # } else {
+  #   allcorners = c(allcorners,corners)
+  #   allcenters = c(allcenters,centers)
+  # }
+  
+  ## number them incrementally
+  allcenters = st_as_sf(allcenters)
+  coords = st_coordinates(allcenters)
+  allcenters$xcoord = coords[,1]
+  allcenters$ycoord = coords[,2]
+  
+  allcenters = allcenters %>%
+    mutate(stemmap_id = stemmap_name) %>%
+    arrange(desc(ycoord),xcoord) %>%
+    mutate(xcoord_int = match(xcoord, unique(xcoord)),
+           ycoord_int = match(ycoord, unique(ycoord))) %>%
+    mutate(name = paste0("p",xcoord_int,"-",ycoord_int))
+  
+  ## number them incrementally
+  allcorners = st_as_sf(allcorners)
+  coords = st_coordinates(allcorners)
+  allcorners$xcoord = coords[,1]
+  allcorners$ycoord = coords[,2]
+  
+  allcorners = allcorners %>%
+    mutate(stemmap_id = stemmap_name) %>%
+    arrange(desc(ycoord),xcoord) %>%
+    mutate(xcoord_int = match(xcoord, unique(xcoord)),
+           ycoord_int = match(ycoord, unique(ycoord))) %>%
+    mutate(name = paste0("v",1:nrow(allcorners)))
+  
+  allpoints = rbind(allcorners,allcenters)
+  
+  # add z dimension
+  allpoints = st_zm(allpoints, drop=FALSE, what = "Z")
+  allpoints = allpoints %>% st_transform(4326)
+  
+  st_write(allcenters,paste0("/home/derek/Documents/data/nyfp-stand-mapping_data/stem-map-layout/stemmap_",stemmap_name,"_centers.kml"),delete_dsn = TRUE)
+  st_write(allcorners,paste0("/home/derek/Documents/data/nyfp-stand-mapping_data/stem-map-layout/stemmap_",stemmap_name,"_corners.kml"),delete_dsn = TRUE)
+  st_write(allpoints,paste0("/home/derek/Documents/data/nyfp-stand-mapping_data/stem-map-layout/stemmap_",stemmap_name,"_grid.geojson"),delete_dsn = TRUE)
+  
+  # write all points as csv in emlid format
+  d = st_transform(allpoints,4326)
+  coords = st_coordinates(d)
+  d$Easting = coords[,1]
+  d$Northing = coords[,2]
+  d$Elevation = 0
+  d$Name = d$name
+  st_geometry(d) = NULL
+
+  d = d %>%
+    select(Name,Easting,Northing,Elevation)
+  
+  write.csv(d,paste0("/home/derek/Documents/data/nyfp-stand-mapping_data/stem-map-layout/stemmap_",stemmap_name,"_grid.csv"),row.names=FALSE, quote=FALSE)
+  
+    
+}
+
+
+
+
+
+
