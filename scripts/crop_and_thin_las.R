24b27040eeab5bd0078c491fdf884bece6b63a70
Derek Young <derek.j.n.young@gmail.com>
2021-12-04 15:57:46 -0800

    Skip LAS normalization

--- scripts/crop_and_thin_las.R
+++ scripts/crop_and_thin_las.R
@@ -1,5 +1,5 @@
 library(sf)
-library(dplyr)
+library(tidyverse)
 library(lidR)
 #library(furrr)
 
@@ -11,28 +11,28 @@ library(lidR)
 
 focal_area = st_read("/media/djyoung/Latimer_Drone_Data/tnc-nyfp/focal-area-mask/study-area-masks/flight-units.gpkg") %>% st_transform(3310)
 
-
-#### DTM
-
-dtm = raster("/media/djyoung/Latimer_Drone_Data/tnc-nyfp/usgs_dem.tif") %>% projectRaster(crs = "+proj=aea +lat_1=34 +lat_2=40.5 +lat_0=0 +lon_0=-120 +x_0=0 +y_0=-4000000 +ellps=GRS80 +datum=NAD83 +units=m +no_defs ")
-
-## Disaggregation of DEM
-
-# skip if file aleady exists
-dem_file = "/media/djyoung/Latimer_Drone_Data/tnc-nyfp/usgs_dem_interp.tif"
-if(file.exists(dem_file)) {
-  cat("Already exists:",dem_file,". Loading from already created.\n")
-} else {
-
-  cat("\nstarting disaggregation\n")
-  # interpolate the the DEM to the same res as the finest CHMs (0.05 m)
-  dtm_interp = disaggregate(dtm,fact=ceiling(10/0.05), method="bilinear") # starting res is 10 m
-  cat("completed disaggregation\n")
-  
-  writeRaster(dtm_interp,dem_file)
-}
-
-dtm_interp = raster(dem_file)
+# 
+# #### DTM
+# 
+# dtm = raster("/media/djyoung/Latimer_Drone_Data/tnc-nyfp/usgs_dem.tif") %>% projectRaster(crs = "+proj=aea +lat_1=34 +lat_2=40.5 +lat_0=0 +lon_0=-120 +x_0=0 +y_0=-4000000 +ellps=GRS80 +datum=NAD83 +units=m +no_defs ")
+# 
+# ## Disaggregation of DEM
+# 
+# # skip if file aleady exists
+# dem_file = "/media/djyoung/Latimer_Drone_Data/tnc-nyfp/usgs_dem_interp.tif"
+# if(file.exists(dem_file)) {
+#   cat("Already exists:",dem_file,". Loading from already created.\n")
+# } else {
+# 
+#   cat("\nstarting disaggregation\n")
+#   # interpolate the the DEM to the same res as the finest CHMs (0.05 m)
+#   dtm_interp = disaggregate(dtm,fact=ceiling(10/0.05), method="bilinear") # starting res is 10 m
+#   cat("completed disaggregation\n")
+#   
+#   writeRaster(dtm_interp,dem_file)
+# }
+# 
+# dtm_interp = raster(dem_file)
 
 ## get las layers from metashape outputs directory
 las_files = list.files("/media/djyoung/Latimer_Drone_Data/tnc-nyfp/metashape-products",pattern=".*\\.las", full.names=TRUE)  # to filter to ones matching a name: pattern=paste0(las_layer_name,".*\\.las")
@@ -65,7 +65,7 @@ crop_and_write_las = function(las_file) {
   las = clip_roi(las,focal_area)
   las = filter_duplicates(las)
   las = decimate_points(las, homogenize(50,5))
-  las = normalize_elevation(las = las, algorithm = dtm_interp, na.rm = TRUE)
+  # las = normalize_elevation(las = las, algorithm = dtm_interp, na.rm = TRUE)
 
 
   writeLAS(las,filename)
