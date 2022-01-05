05540a148cdba365a5067de18ec457df047980ef
Derek Young <derek.j.n.young@gmail.com>
2022-01-04 22:21:34 -0800

    Add segmentation

--- scripts/detect_trees.R
+++ scripts/detect_trees.R
@@ -8,6 +8,7 @@ library(ForestTools)
 library(tictoc)
 library(sf)
 library(furrr)
+library(lidR)
 
 #### Get data dir ####
 # The root of the data directory
@@ -21,6 +22,8 @@ source(here("scripts/convenience_functions.R"))
 
 chm_files = list.files(datadir("drone-data/chms"), full.names = TRUE)
 ttop_units = list()
+canopy_units_mcws = list()
+canopy_units_silva = list()
 
 for(j in 1:length(chm_files)) {
   
@@ -59,8 +62,7 @@ for(j in 1:length(chm_files)) {
   
   plan(multisession,workers=10)
   ttop_tiles = future_map(tiles, vwf, winFun = lin_196, minHeight=5, maxWinDiameter=99)
-  
-  
+
   ## crop each raster back to original tile extent
   grid_sf = st_as_sf(grid)
   ttop_tiles_sf = lapply(ttop_tiles,st_as_sf)
@@ -75,17 +77,36 @@ for(j in 1:length(chm_files)) {
   # merge the tiles back together
   ttops = bind_rows(ttop_tiles_crop)
   
+  ttops_sp = ttops %>% as("Spatial")
+  ttops_sp$treeID = 1:nrow(ttops_sp)
+  
+  canopies_mcws = mcws(ttops_sp,raster(chm_smooth),minHeight = 10)
+  canfun = silva2016(raster(chm_smooth), ttops_sp, max_cr_factor = 0.24, exclusion = 0.1, ID = "treeID")
+  canopies_silva = canfun()
+  
+  canopies_rast_mcws = rast(canopies_mcws)
+  canopies_poly_mcws = as.polygons(canopies_rast_mcws) %>% st_as_sf
+  
+  canopies_rast_silva = rast(canopies_silva)
+  canopies_poly_silva = as.polygons(canopies_rast_silva) %>% st_as_sf
+  
+  
   # buffer in from the original perim
   ttops = st_intersection(ttops,perim %>% st_buffer(-10))
 
   ttop_units[[j]] = ttops
+  canopy_units_mcws[[j]] = canopies_poly_mcws
+  canopy_units_silva[[j]] = canopies_poly_silva
 }
 
 ttops_merged = bind_rows(ttop_units)
-
+canopies_merged_mcws = bind_rows(canopy_units_mcws)
+canopies_merged_silva = bind_rows(canopy_units_silva)
 
 
 
 ## write the detected trees
 
 st_write(ttops_merged,datadir("drone-data/detected-trees/ttops-vwf196.gpkg"), delete_dsn=TRUE)
+st_write(canopies_merged_mcws,datadir("drone-data/detected-trees/canopies-vwf196_mcws.gpkg"), delete_dsn=TRUE)
+st_write(canopies_merged_silva,datadir("drone-data/detected-trees/canopies-vwf196_silva.gpkg"), delete_dsn=TRUE)
